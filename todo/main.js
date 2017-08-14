c5b37a85bbec2ccbf6d9330d28e3f205b7bddf5e
John Lopez <no-reply>
2017-08-14 01:29:01 -0700

    First commit

---
+++ todo/main.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+
+    /* Remove and marker icons in SVG */
+
+    var removeSVG = '<svg width="14px" height="18px" viewBox="0 0 14 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><!-- Generator: Sketch 46.1 (44463) - http://www.bohemiancoding.com/sketch --><title>ic_delete</title><desc>Created with Sketch.</desc><defs></defs><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Action" transform="translate(-53.000000, -291.000000)"><g id="ic_delete" transform="translate(48.000000, 288.000000)"><polygon id="Bounds" points="0 0 24 0 24 24 0 24"></polygon><path class="fill" d="M6,19 C6,20.1 6.9,21 8,21 L16,21 C17.1,21 18,20.1 18,19 L18,7 L6,7 L6,19 L6,19 Z M19,4 L15.5,4 L14.5,3 L9.5,3 L8.5,4 L5,4 L5,6 L19,6 L19,4 L19,4 Z" id="Icon" fill="#000000"></path></g></g></g></svg>'
+    var completeSVG = '<svg class="unmarked" width="18px" height="18px" viewBox="0 0 18 18" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><!-- Generator: Sketch 46.1 (44463) - http://www.bohemiancoding.com/sketch --><title>ic_check_box_outline_blank</title><desc>Created with Sketch.</desc><defs></defs><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="Toggle" transform="translate(-99.000000, -51.000000)"><g id="ic_check_box_outline_blank" transform="translate(96.000000, 48.000000)"><polygon id="Bounds" points="0 0 24 0 24 24 0 24"></polygon><path class="fill" d="M19,5 L19,19 L5,19 L5,5 L19,5 L19,5 Z M19,3 L5,3 C3.9,3 3,3.9 3,5 L3,19 C3,20.1 3.9,21 5,21 L19,21 C20.1,21 21,20.1 21,19 L21,5 C21,3.9 20.1,3 19,3 L19,3 Z" id="Icon" fill="#000000"></path></g></g></g></svg>'
+
+    
+    function buttonClick() {
+        var value = document.getElementById('item').value;
+        if (value) {
+            addItemTodo(value);
+            document.getElementById('item').value = '';
+        }
+    };
+     /* User clicked Add button. Text in the item field is added to a list. */
+    document.getElementById('add').addEventListener('click', buttonClick) ;
+
+    function removeItem() {
+        console.log('this');
+    }
+
+    /* Adds new item to Todo List */
+    function addItemTodo(text) {
+        var list = document.getElementById('todo');
+
+        var item = document.createElement('li');
+        item.innerText = text;
+
+        var buttons = document.createElement('div');
+        buttons.classList.add('buttons');
+
+        var remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.innerHTML = removeSVG;
+
+        //Click event for removing an item.
+        remove.addEventListener('click', removeItem);
+
+
+        var complete = document.createElement('button');
+        complete.classList.add('complete');
+        complete.innerHTML = completeSVG;
+
+        buttons.appendChild(remove);
+        buttons.appendChild(complete);
+        item.appendChild(buttons);
+
+        list.insertBefore(item, list.childNodes[0]);
+    }
+
+}
